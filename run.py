fb907205864413bb22b419aa4db5cc735b426927
yichenlv <yichenlv@foxmail.com>
2025-11-19 21:58:09 +0800

    1. 设置数据集

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
+# 命令方式训练
+# yolo task=detect mode=train model=yolov8n.pt  data=yolo.cattle.yml epochs=30  workers=1 batch=2
+
+
 from ultralytics import YOLO
 
-# 加载一个预训练的 YOLO11n 模型
-yolo = YOLO(model="yolov8n.pt",task="detect")
+# # 加载一个预训练的 YOLO11n 模型
+# yolo = YOLO(model="yolov8n.pt",task="detect")
+#
+# result = yolo(source="ultralytics/assets/bus.jpg")
+# # result = yolo(source=0) # 通过摄像头检测
 
-result = yolo(source="ultralytics/assets/bus.jpg")
 
-# result = yolo(source=0) # 通过摄像头检测
+yolo = YOLO("yolov8n.pt")
+yolo.train(data='yolo_cattle.yml',workers=0,epochs=1, batch=2)
